017b3ebc45cb1817cb2ac502a27cba3e1b19eb75
Felix Huang <yuehuang010@gmail.com>
2026-04-28 17:38:21 -0700

    Update buildAndRun to accept test file.

--- buildAndRun.bat
+++ buildAndRun.bat
@@ -1,6 +1,12 @@
 @echo off
 setlocal
-REM set PATH=%CD%\vcpkg_installed\x64-windows-static\x64-windows-static\tools\llvm;%PATH%
-msbuild MyCompiler.slnx -p:Platform=x64 -p:Configuration=Debug -v:minimal && C:\source\MyCompiler\x64\Debug\MyCompiler.exe  --nologo Test\test_block_allocator.cb -o myapp.exe --out-lli out.ll -p win64 %*
+
+set TESTFILE=%1
+if "%TESTFILE%"=="" set TESTFILE=test_basic.cb
+
+msbuild MyCompiler.slnx -p:Platform=x64 -p:Configuration=Debug -v:minimal && (
+    C:\source\MyCompiler\x64\Debug\MyCompiler.exe --nologo Test\%TESTFILE% -o out\myapp.exe --out-lli out.ll -p win64
+    if not errorlevel 1 out\myapp.exe
+)
 
 endlocal
